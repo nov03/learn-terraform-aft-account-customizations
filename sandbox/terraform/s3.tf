15d7f4d83b67291d20e599dbae811aadf51fd3ed
ishii <ishii@serverworks.co.jp>
2023-07-21 08:50:37 +0900

    asdf

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "privateA"
+  bucket = "aftAA-sandbox-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
 }
